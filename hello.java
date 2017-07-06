2062b3a5ee42c5e9b61b35bb44e3e71c521d5706
aswin1617 <saaswin1617@gmail.com>
2017-07-06 14:58:30 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class hello
+{
+ public static void main(String args[]) throws IOException 
+ { 
+
+  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+ 
+  for(int i=0;i<5;i++)
+  System.out.println("Hello");
+  }
+  }
+  
