b8a515b21ccc1466caedb99abe7cae0946e2ae20
aswin1617 <saaswin1617@gmail.com>
2017-07-06 14:40:36 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class largest
+{
+ public static void main(String args[]) throws IOException
+ { 
+  int a,b,c;
+  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+  a=Integer.parseInt(br.readLine());
+  b=Integer.parseInt(br.readLine());
+  c=Integer.parseInt(br.readLine());
+  if(a>b && a>c)
+  System.out.println(a);
+  else if(b<a && b<c)
+  System.out.println(b);
+  else
+  System.out.println(c);
+  }
+  }
