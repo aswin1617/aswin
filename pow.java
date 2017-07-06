4ac5f40bea190664395e7f12c8a231cc189ba199
aswin1617 <saaswin1617@gmail.com>
2017-07-06 16:54:52 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+public class pow{
+  public static void main(String arg[])throws IOException 
+  {
+    int n,p,result=1;
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    n=Integer.parseInt(br.readLine());
+    p=Integer.parseInt(br.readLine());
+    if(n>=0&&p==0)
+     {
+        result=1;
+     }
+    else if(n==0&&p>=1)
+      { 
+         result=0;
+      }
+    else
+     {
+         for(int i=1;i<=p;i++)
+   {
+            result=result*n;
+   }      
+     }
+     System.out.println(result);
+     }
+     }
