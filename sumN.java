2a58c4101b546cb08ee9d0396ca73c4e97b3f89f
aswin1617 <saaswin1617@gmail.com>
2017-07-06 15:13:40 +0530

    Create sumN.java

---
+++ sumN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SumN 
+ { 
+     public static void main(String[] args) 
+     { 
+        int n,i,sum=0; 
+        Scanner in=new Scanner(System.in); 
+        n=in.nextInt(); 
+        for(i=0;i<n;i++)
+                 sum=sum+i; 
+                 System.out.print(sum); 
+     } 
+      
+} 
