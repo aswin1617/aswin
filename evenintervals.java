a820de48adbfd7b8d073778f9a92d00eef528cc4
aswin1617 <saaswin1617@gmail.com>
2017-07-08 09:29:03 +0530

    Create evenintervals.java

---
+++ evenintervals.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class oddnumbersinterval
+{  
+  public static void main(String[] args)throws IOException 
+   {  
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     int n,i,r;
+     n=Integer.parseInt(br.readLine());
+     r=Integer.parseInt(br.readLine());
+     for(i=n;i<r;i++)
+     {
+       if(i%2==0)
+       {
+         System.out.println(i);
+         }
+         }
+       
+   }  
+}
